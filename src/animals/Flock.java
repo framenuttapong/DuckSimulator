5abf23eca122b6aee14d33343029900b2f89c6c6
20♥ <nuttapong.n@ku.th>
2022-08-20 16:22:57 +0700

    finish

---
+++ src/animals/Flock.java
@@ -0,0 +1,18 @@
+package animals;
+
+import java.util.ArrayList;
+
+public class Flock implements Quackable {
+    ArrayList<Quackable> quackers = new ArrayList<>();
+
+    public void add(Quackable quacker) {
+        quackers.add(quacker);
+    }
+
+    @Override
+    public void quack() {
+        for (Quackable quacker : quackers) {
+            quacker.quack();
+        }
+    }
+}
